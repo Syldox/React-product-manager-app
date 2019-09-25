62d9496e55e1be7cb0392cb7327261faede558f6
Sylla Ismaila <syldox@gmail.com>
2019-09-25 11:17:28 +0200

    minore change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Header from './components/Header';
-// import Contacts from './components/Contacts';
 import Products from './components/Products';
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
     <div>
       <Header branding = "Product-Manager"/>
       <div className="container">
-      {/* <Contacts/> */}
       <Products/>
       </div>
     </div>
